94c7bc5d9ff338389d70029a213bb10fcd3f58dc
rossijv <posttestinglab@gmail.com>
2026-04-16 11:28:05 -0400

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,6 +1,17 @@
 export async function onRequest(context) {
-  const USER = "VISITOR";
-  const PASS = "CANDIDATEPORTAL"; // troque isso depois
+  const url = new URL(context.request.url);
+
+  // 🔒 só protege a pasta /fifa2026
+  const isProtectedRoute = url.pathname.startsWith("/florianacoach");
+
+  // se NÃO for essa pasta, deixa passar direto
+  if (!isProtectedRoute) {
+    return context.next();
+  }
+
+  // 👇 proteção só para /florianacoach
+  const USER = "FLORIANA";
+  const PASS = "COACHDOANO";
 
   const auth = context.request.headers.get("Authorization");
 
