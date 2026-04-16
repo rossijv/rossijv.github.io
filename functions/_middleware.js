c3ca860792479e992c45160903b586d0a114b593
rossijv <posttestinglab@gmail.com>
2026-04-16 11:19:49 -0400

    Update _middleware.js

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,1 +1,20 @@
+export async function onRequest(context) {
+  const USER = "VISITOR";
+  const PASS = "CANDIDATEPORTAL"; // troque isso depois
 
+  const auth = context.request.headers.get("Authorization");
+
+  const expected =
+    "Basic " + btoa(`${USER}:${PASS}`);
+
+  if (auth === expected) {
+    return context.next();
+  }
+
+  return new Response("Auth required", {
+    status: 401,
+    headers: {
+      "WWW-Authenticate": 'Basic realm="Private Area"',
+    },
+  });
+}
